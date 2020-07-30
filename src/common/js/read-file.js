ed967681e672e4d7b74dd8c650313c3aba07a284
孙华鹏 <sunhuapeng@heydayscape.com>
2020-07-30 13:43:20 +0800

    create project

---
+++ src/common/js/read-file.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: sunhuapeng
+ * @Date: 2020-07-30 13:34:27
+ * @LastEditors: sunhuapeng
+ * @LastEditTime: 2020-07-30 13:41:11
+ */
+
+class ReadFile {
+  constructor() {
+    
+  }
+}
